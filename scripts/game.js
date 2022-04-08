01395e945004a7e6e57f760039010ea979b88922
Samuel Livramento Linux <samuellivramento122@outlook.com>
2022-04-08 13:05:49 -0300

    Adicionando algumas boas práticas

--- scripts/game.js
+++ scripts/game.js
@@ -16,49 +16,49 @@ let game = {
         'react'
     ],
     setCard(id) {
-        let card = this.cards.filter(card => card.id === id)[0]
+        let card = this.cards.filter(card => card.id === id)[0];
         if (card.flipped || this.lockMode) {
-            return false
+            return false;
         }
 
         if (!this.firstCard) {
-            this.firstCard = card
-            this.firstCard.flipped = true
-            return true
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
         } else {
-            this.secondCard = card
-            this.secondCard.flipped = true
-            this.lockMode = true
-            return true
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
         }
     },
     checkMatch() {
-        return (!this.firstCard || !this.secondCard) ? false : this.firstCard.icon === this.secondCard.icon
+        return (!this.firstCard || !this.secondCard) ? false : this.firstCard.icon === this.secondCard.icon;
     },
     unflipCards() {
-        this.firstCard.flipped = false
-        this.secondCard.flipped = false
-        this.clearCards()
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
     },
     checkGameOver() {
-        return this.cards.filter(card => !card.flipped).length === 0
+        return this.cards.filter(card => !card.flipped).length === 0;
     },
     clearCards() {
-        this.firstCard = null
-        this.secondCard = null
-        this.lockMode = null
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = null;
     },
     createCardsFromTechs() {
-        this.cards = []
+        this.cards = [];
 
         for (let tech of this.techs) {
-            this.cards.push(this.createPairFromTech(tech))
+            this.cards.push(this.createPairFromTech(tech));
         }
 
-        this.cards = this.cards.flatMap(pair => pair)
-        this.shuffleCards()
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
 
-        return this.cards
+        return this.cards;
     },
 
     createPairFromTech(tech) {
@@ -74,17 +74,17 @@ let game = {
     },
 
     createId(txt) {
-        return txt + parseInt(Math.random() * 1000)
+        return txt + parseInt(Math.random() * 1000);
     },
 
     shuffleCards() {
-        let currentIndex = this.cards.length
-        let randomIndex = 0
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
 
         while (currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex)
-            currentIndex--
-            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
         }
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,84 +1,84 @@
-const FRONT = 'cardFront'
-const BACK = 'cardBack'
-const CARD = 'card'
-const ICON = 'icon'
-let motions = 0
-let dataPlayer = {}
+const FRONT = 'cardFront';
+const BACK = 'cardBack';
+const CARD = 'card';
+const ICON = 'icon';
+let motions = 0;
+let dataPlayer = {};
 
 document.getElementById('playButton').addEventListener('click', (e) => {
-    e.preventDefault()
-    let playerName = document.getElementById('playerName').value.trim()
+    e.preventDefault();
+    let playerName = document.getElementById('playerName').value.trim();
     if (playerName !== '') {
-        let startGameLayer = document.getElementById('startGameView')
-        startGameLayer.style.display = 'none'
-        dataPlayer.id = game.createId(playerName)
-        dataPlayer.name = playerName
-        dataPlayer.motions = null
-        startGame()
+        let startGameLayer = document.getElementById('startGameView');
+        startGameLayer.style.display = 'none';
+        dataPlayer.id = game.createId(playerName);
+        dataPlayer.name = playerName;
+        dataPlayer.motions = null;
+        startGame();
     } else {
-        alert('Por favor, digite um nome!')
+        alert('Por favor, digite um nome!');
     }
 })
 
 function startGame() {
-    initializeCards(game.createCardsFromTechs())
+    initializeCards(game.createCardsFromTechs());
 }
 
 function initializeCards(cards) {
-    let gameBoard = document.getElementById('gameBoard')
-    gameBoard.innerHTML = ''
+    let gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
     game.cards.forEach(card => {
-        let cardElement = document.createElement('div')
-        cardElement.id = card.id
-        cardElement.classList.add(CARD)
-        cardElement.dataset.icon = card.icon
-        createCardContent(card, cardElement)
-        cardElement.addEventListener('click', flipCard)
-        gameBoard.appendChild(cardElement)
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardContent(card, cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
     })
 }
 
 function createCardContent(card, cardElement) {
-    createCardFace(FRONT, card, cardElement)
-    createCardFace(BACK, card, cardElement)
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
 }
 
 function createCardFace(face, card, element) {
-    let cardElementFace = document.createElement('div')
-    cardElementFace.classList.add(face)
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
     if (face === FRONT) {
-        let iconElement = document.createElement('img')
-        iconElement.classList.add(ICON)
-        iconElement.src = `./assets/images/${card.icon}.png`
-        cardElementFace.appendChild(iconElement)
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = `./assets/images/${card.icon}.png`;
+        cardElementFace.appendChild(iconElement);
     } else {
-        cardElementFace.innerHTML = '&lt;/&gt;'
+        cardElementFace.innerHTML = '&lt;/&gt;';
     }
-    element.appendChild(cardElementFace)
+    element.appendChild(cardElementFace);
 }
 
 function flipCard() {
     if (game.setCard(this.id)) {
-        this.classList.add('flip')
+        this.classList.add('flip');
         if (game.secondCard) {
-            motions++
+            motions++;
             if (game.checkMatch()) {
-                game.clearCards()
+                game.clearCards();
                 if (game.checkGameOver()) {
-                    let gameOverLayer = document.getElementById('gameOverView')
-                    let motionUserView = document.getElementById('motionsP')
-                    motionUserView.innerHTML = `Você fez ${motions} movimentos para finalizar o game`
-                    gameOverLayer.style.display = 'flex'
-                    recoverPlayerScore()
+                    let gameOverLayer = document.getElementById('gameOverView');
+                    let motionUserView = document.getElementById('motionsP');
+                    motionUserView.innerHTML = `Você fez ${motions} movimentos para finalizar o game`;
+                    gameOverLayer.style.display = 'flex';
+                    recoverPlayerScore();
                 }
             } else {
                 setTimeout(() => {
-                    let firstCardView = document.getElementById(game.firstCard.id)
-                    let secondCardView = document.getElementById(game.secondCard.id)
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
 
-                    firstCardView.classList.remove('flip')
-                    secondCardView.classList.remove('flip')
-                    game.unflipCards()
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
                 }, 1000)
             }
         }
@@ -86,77 +86,77 @@ function flipCard() {
 }
 
 function restart() {
-    game.clearCards()
-    startGame()
-    let gameOverLayer = document.getElementById('gameOverView')
-    gameOverLayer.style.display = 'none'
-    motions = 0
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById('gameOverView');
+    gameOverLayer.style.display = 'none';
+    motions = 0;
 }
 
 /* Minhas modificações */
 function recoverPlayerScore() {
-    let scores = []
-    dataPlayer.motions = motions
+    let scores = [];
+    dataPlayer.motions = motions;
 
     //VERIFICA E RECUPERA
     if (localStorage.length !== 0) {
         while (scores.length !== localStorage.length) {
-            let score = JSON.parse(localStorage.getItem(scores.length + 1))
-            scores.push(score)
+            let score = JSON.parse(localStorage.getItem(scores.length + 1));
+            scores.push(score);
         }
     }
 
     if (localStorage.length < 5) {
         //adiciona player atual
-        scores.push(dataPlayer)
+        scores.push(dataPlayer);
     } else {
-        scores.reverse() //Organizar na ordem decrescente
+        scores.reverse(); //Organizar na ordem decrescente
         for (let i = 0; i < scores.length; i++) {
             //Verifica se algum motions é menor que o outro
             if (dataPlayer.motions < scores[i].motions) {
-                scores.splice(i, 1, dataPlayer)
-                break
+                scores.splice(i, 1, dataPlayer);
+                break;
             }
         }
     }
-    organizeArray(scores)
-    savePlayers(scores)
-    listPlayersView(scores)
+    organizeArray(scores);
+    savePlayers(scores);
+    listPlayersView(scores);
 }
 
 function organizeArray(arrScores) {
-    arrScores.sort((a, b) => a.motions - b.motions)
+    arrScores.sort((a, b) => a.motions - b.motions);
 }
 
 function savePlayers(arrScores) {
-    localStorage.clear()
+    localStorage.clear();
     arrScores.forEach((score, i) => {
-        localStorage.setItem(i + 1, JSON.stringify(score))
+        localStorage.setItem(i + 1, JSON.stringify(score));
     })
 }
 
 // mostrando ranking para o jogador
 function listPlayersView(playersScores) {
-    let tableScores = document.getElementById('rankingTable').children['1'] // acessando o tbody
-    tableScores.innerHTML = ''
+    let tableScores = document.getElementById('rankingTable').children['1']; // acessando o tbody
+    tableScores.innerHTML = '';
     if(playersScores){
         playersScores.forEach((player, indexPlayer) => {
-            let tRow = document.createElement('tr')
+            let tRow = document.createElement('tr');
             for (let prop in player) {
-                let tData = document.createElement('td')
+                let tData = document.createElement('td');
                 if (prop === 'id') {
-                    tData.innerHTML = `${indexPlayer + 1}°`
+                    tData.innerHTML = `${indexPlayer + 1}°`;
                 } else {
-                    tData.innerHTML = player[prop]
+                    tData.innerHTML = player[prop];
                 }
-                tRow.appendChild(tData)
+                tRow.appendChild(tData);
             }
-            tableScores.appendChild(tRow)
+            tableScores.appendChild(tRow);
         })
     }
 }
 
 function restartRanking(){
-    localStorage.clear()
-    listPlayersView()
+    localStorage.clear();
+    listPlayersView();
 }
